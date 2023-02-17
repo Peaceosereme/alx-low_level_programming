63ecbd2be2052d8aa31803d744318366b388216f
Peaceosereme <peaceeromonsele@gmail.com>
2023-02-17 12:31:04 -0800

    Printing in C with sp characters

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
-/* main and stuff */
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	/* string initialization */
-	puts("\"Programming is like building a multilingual puzzle"\n return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
