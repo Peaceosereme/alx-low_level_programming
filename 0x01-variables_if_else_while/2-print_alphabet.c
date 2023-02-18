9051917163166a071022f346082b959856d05041
Peaceosereme <peaceeromonsele@gmail.com>
2023-02-17 20:49:27 -0800

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stlib.h>
+#include <time.h>
+/**
+ * main - Entry
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]};
+	}
+	putchar('\n');
+	return (0);
+}
