a42081c1280bf088fce401c6e6f1d0a859b7894c
Peaceosereme <peaceeromonsele@gmail.com>
2023-02-18 21:11:16 -0800

    Alphabeting

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int alp;
+	int alpp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+	{
+		putchar(alp);
+	}
+	for (alpp = 'A'; alp <= 'Z'; alp++)
+	{
+		putchar(alpp);
+	}
+	return (0);
+}
