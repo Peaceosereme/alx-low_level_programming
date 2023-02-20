51c0e8c916aa7e176a9852a78db75499f25cee31
Peaceosereme <peaceeromonsele@gmail.com>
2023-02-19 20:01:40 -0800

    Printing numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num = '0';
+
+	for (num = '0'; num < 10; num++)
+		printf("%d\n", num);
+	return (0);
+}
