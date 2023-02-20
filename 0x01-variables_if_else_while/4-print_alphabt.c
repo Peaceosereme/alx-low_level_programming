fac633929c56f12b369610adf2c9e766c4714c38
Peaceosereme <peaceeromonsele@gmail.com>
2023-02-19 19:52:29 -0800

    Alphabet with exceptions

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alp = 'a';
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+		if (alp != 'q' && alp != 'a')
+			putchar(alp);
+	putchar('\n');
+	return (0);
+}
