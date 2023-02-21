793962d97263e9d24e8f06d57cfe3f42325509b9
Peaceosereme <peaceeromonsele@gmail.com>
2023-02-20 19:17:11 -0800

    Hexadecimals

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int d;
+	int l;
+
+	for (d = 0; d <= 9; d++)
+		putchar(d);
+	l = 'a';
+	for (l = 'a'; l <= 'f'; l++)
+		putchar(l);
+	putchar('\n');
+	return (0);
+}
