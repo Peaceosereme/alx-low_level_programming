9d177275bb4e50d752f52a62855714a915e3a3b9
Peaceosereme <peaceeromonsele@gmail.com>
2023-02-20 20:11:31 -0800

    Combo but not combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int s;
+
+	for (s = 0; s < 10; s++)
+	{
+		putchar(s);
+		if (s < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
