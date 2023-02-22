b76f6259d2f956bcc65957a79a6ccd4c062a960d
Peaceosereme <peaceeromonsele@gmail.com>
2023-02-21 20:35:49 -0800

    Printing alph with new fxn

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		_putchar(alp[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
