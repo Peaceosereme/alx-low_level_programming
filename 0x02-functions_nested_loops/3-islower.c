5778c7c566a1387af46c2ddfad6787586bafd295
Peaceosereme <peaceeromonsele@gmail.com>
2023-02-22 21:00:07 -0800

    Lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _islower - checks for lowercase characters.
+ *
+ * Return: Always 0.
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <+ 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
