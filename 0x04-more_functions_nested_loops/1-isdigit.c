29cb69d815f74134253decbd96bdf56d125663a2
Peaceosereme <peaceeromonsele@gmail.com>
2023-02-23 19:13:34 -0800

    CHecking if digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if c is a single digit.
+ * @c: integer tested
+ * Return: 1 if digit, 0. if other
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
