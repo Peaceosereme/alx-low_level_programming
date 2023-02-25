b023117f8feb2aa8cef98231127c3fc48aa6596f
Peaceosereme <peaceeromonsele@gmail.com>
2023-02-24 19:27:55 -0800

    Checking for a digit

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,13 +2,14 @@
 
 /**
  * mul - multiplies a and b.
- * @a: first integer tested; @b: 2nd integer
+ * @a: first integer tested
+ * @b: 2nd integer
  * Return:0
  */
 
 int mul(int a, int b)
 {
-	int mult = a * b;
+	int mult = ((a * b));
 
 	_putchar(mult + '0');
 	return (0);
