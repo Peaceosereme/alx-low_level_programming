5638aff802a33f66a9681ad195f9190139484f2c
Peaceosereme <peaceeromonsele@gmail.com>
2023-02-28 17:21:10 -0800

    Printing string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,12 +8,10 @@
 
 void _puts(char *str)
 {
-	int i = 0;
-	int c;
-
-	while ((c = *(str + i) != '\0'))
+	while (*str != '\0')
 	{
-	_putchar(c);
-	i++;
+		_putchar(*str);
+		str++;
 	}
+	_putchar('\n');
 }
