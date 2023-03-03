1c1943b903d6a0323c32d0a82a36d574b598a191
Peaceosereme <peaceeromonsele@gmail.com>
2023-03-03 15:29:07 -0800

    Replacemeeents in a string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *leet - replacemeents, get the replacemeents
+ * @s: string to be tampered with
+ * Return: Always 0.
+ */
+
+char *leet(char *s)
+{
+	int a;
+	int b;
+	char l[] = "aeotl";
+	char u[] = "AEOTL";
+	char n[] = "43071";
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		for (b = 0; l[b] != '\0' && u[b] != '\0'; b++)
+		{
+			if (s[a] == l[b] || s[a] == u[b])
+			{
+				s[a] = n[b];
+			}
+		}
+	}
+	return (s);
+}
