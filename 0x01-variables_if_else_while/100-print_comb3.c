98539d317ada2c5c02426e11c8bebf0a9f28b136
Peaceosereme <peaceeromonsele@gmail.com>
2023-03-25 12:31:28 -0700

    2 digit combo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,5 +9,23 @@
 
 int main(void)
 {
-	int i 
+	int i, j;
+
+	for (i = 0; i < 9; i++)
+	{
+		j = i + 1;
+		while (j < 10)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+	}
+	putchar('\n');
+	return (0);
 }
